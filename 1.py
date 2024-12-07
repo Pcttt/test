6f05cb7c0a455f25f38b34572d1f152dde1eaa6b
Pcttt <panchita.chan@gmail.com>
2024-12-07 14:29:06 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -52,7 +52,7 @@ def main():
     st.markdown(
     """
     <h1 style="text-align: center; font-size: 2.5em;">
-        🌸 <span style="border-bottom: 5px solid #F8BBD0;">Learn Japanese with Kanbun Poetry</span> 🌸
+        🌸 Learn Japanese with <span style="border-bottom: 3px solid #F8BBD0;">Kanbun Poetry</span> 🌸
     </h1>
     """,
     unsafe_allow_html=True
